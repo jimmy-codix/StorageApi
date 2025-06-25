0e2c1652054c60fbfe97d185387073168f6a852b
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-25 16:45:35 +0200

    Added DTOs.

---
+++ StorageApi/DTOs/CreateProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace StorageApi.DTOs
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public string Shelf { get; set; } = string.Empty;
+        public int Count { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
