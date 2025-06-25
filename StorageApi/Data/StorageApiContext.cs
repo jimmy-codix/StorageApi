0db28e467d50d267fe1c310cb1862ff56c06d793
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-25 16:31:02 +0200

    Added seed data.

--- StorageApi/Data/StorageApiContext.cs
+++ StorageApi/Data/StorageApiContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using StorageApi.Models;
 
@@ -15,5 +16,18 @@ namespace StorageApi.Data
         }
 
         public DbSet<StorageApi.Models.Product> Product { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id=1, Name="Chair", Category="Furniture", Count=1, Description="Nice chair.", Price=100, Shelf="A2"},
+                new Product { Id=2, Name="Tire", Category="Vehicle", Count=5, Description="Goodyear Tire.", Price=500, Shelf="F1"},
+                new Product { Id=3, Name="Shirt", Category="Clothes", Count=5, Description="Blue shirt.", Price=25, Shelf="Q8"},
+                new Product { Id=4, Name="Jacket", Category="Clothes", Count=10, Description="Black jacket.", Price=350, Shelf="Q9"},
+                new Product { Id=5, Name="Dog whistle", Category="Animals", Count=100, Description="A whistle for your dog.", Price=10, Shelf="B12"}
+            );
+        }
     }
 }
---
+++ StorageApi/Migrations/20250625141549_Seed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StorageApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Category", "Count", "Description", "Name", "Price", "Shelf" },
+                values: new object[,]
+                {
+                    { 1, "Furniture", 1, "Nice chair.", "Chair", 100, "A2" },
+                    { 2, "Vehicle", 5, "Goodyear Tire.", "Tire", 500, "F1" },
+                    { 3, "Clothes", 5, "Blue shirt.", "Shirt", 25, "Q8" },
+                    { 4, "Clothes", 10, "Black jacket.", "Jacket", 350, "Q9" },
+                    { 5, "Animals", 100, "A whistle for your dog.", "Dog whistle", 10, "B12" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
