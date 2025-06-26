cc7d820967fa470623161ed359b40e25504b79bd
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-26 09:37:29 +0200

    Added. String.Empty.

--- StorageApi/DTOs/ProductDto.cs
+++ StorageApi/DTOs/ProductDto.cs
@@ -3,10 +3,10 @@
     public class ProductDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Price { get; set; }
         public int Count { get; set; }
-        public int InventoryValue => Price * Count;
+        public int InventoryValue => Price * Count; 
 
     }
 }
