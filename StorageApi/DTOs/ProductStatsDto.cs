f34b1e7f26287f09b0a44f90b23e4361b7ccc7e9
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-07-01 17:48:13 +0200

    Added ProductsStatsDto.

---
+++ StorageApi/DTOs/ProductStatsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StorageApi.DTOs
+{
+    public class ProductStatsDto
+    {
+        public int NrOfProducts { get; set; }
+        public decimal InventoryValue { get; set; }
+        public string AveragePrice => (InventoryValue / NrOfProducts).ToString("F2");
+    }
+}
