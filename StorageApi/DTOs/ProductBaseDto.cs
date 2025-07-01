536ee69957b01c8a142ab2f352e0606306bc023a
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-07-01 17:07:13 +0200

    DTO´s now inherrit from ProductBaseDTO.

---
+++ StorageApi/DTOs/ProductBaseDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorageApi.DTOs
+{
+    public abstract class ProductBaseDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Price { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Category { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string Shelf { get; set; } = string.Empty;
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Count { get; set; }
+        [StringLength(1000, MinimumLength = 1)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ StorageApi/DTOs/UpdateProductDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorageApi.DTOs
+{
+    public class UpdateProductDto : ProductBaseDto
+    {
+        [Required]
+        [Range(1,int.MaxValue)]
+        public int Id { get; set; }
+    }
+}
